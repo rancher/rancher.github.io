754620036898de8e291b1f2ed19909a149b03522
lvuch <laurenvuchetich@gmail.com>
2015-12-01 14:43:32 -0700

    Docs nav

--- js/rancher.js
+++ js/rancher.js
@@ -1,54 +1,56 @@
 jQuery(document).ready(function () {
-                       // Set height
-                       var content_height = $(".col-sm-9").height();
-                       $(".col-sm-3").css("min-height", content_height + "px");
-                       
-                       
-                       var url = window.location.pathname;
-                       console.log(url);
-                       //$("ul").find("a[href='" + url + "']").addClass('active');
-                       var parents = $("ul").find("a[href='" + url + "']").parents('ul').length;
-                       if (parents === 2) {
-                       $("ul").find("a[href='" + url + "']").parent().parent().addClass('in');
-                       $("ul").find("a[href='" + url + "']").parent().parent().parent().find('a.collapsed').attr('aria-expanded', 'true');
-                       } else if (parents === 3) {
-                       $("ul").find("a[href='" + url + "']").parent().parent().addClass('in');
-                       var select = $("ul").find("a[href='" + url + "']").parent().parent().attr('id');
-                       $("ul").find("a[href='" + url + "']").parent().parent().parent().parent().addClass('in');
-                       $("ul").find("a[href='" + url + "']").parent().parent().parent().parent().parent().find('a[href=#' + select + ']').addClass('active');
-                       var select = $("ul").find("a[href='" + url + "']").parent().parent().parent().parent().attr('id');
-                       console.log(select);
-                       $("ul").find("a[href='" + url + "']").parent().parent().parent().parent().parent().parent().find('a[href=#' + select + ']').addClass('active');
-                       }
-                       
-                       var destination = $("ul.nav li a").attr("href");
-                       $("ul.nav li a").click(function (event) {
-                                              event.preventDefault();
-                                              var link = $(this);
-                                              var local_destination = link.attr("href");
-                                              $.get(local_destination, function (data) {
-                                                    var title = $(data).find("title").text();
-                                                    var local_content = $(data).find(".col-sm-9").html();
-                                                    changeUrl(title, local_destination);
-                                                    checkActiveState();
-                                                    $(".col-sm-9").html(local_content);
-                                                    //If has a class hash (jump on the page)
-                                                    if(link.hasClass('hash')){
-                                                    var local_destination_array = local_destination.split('#');
-                                                    var paragraph_id = local_destination_array[1];
-                                                    var paragraph_destination = $('.col-sm-9 #' + paragraph_id).offset().top;
-                                                    $('body,html').animate({scrollTop: paragraph_destination + "px"});
-                                                    }
-                                                    // Set height
-                                                    var content_height = $(".col-sm-9").height();
-                                                    $(".col-sm-3").css("min-height", content_height + "px");
-                                                    });
-                                                    setTimeout(function() {
-                                                      linkAnchors();
-                                                    },250);
-                                              });
-                       linkAnchors();
-                       });
+  checkActiveState();
+
+  // Set height
+  var content_height = $(".col-sm-9").height();
+  $(".col-sm-3").css("min-height", content_height + "px");
+
+  $("ul.nav li a").click(function (event) {
+    var link = $(this);
+    var local_destination = link.attr("href");
+
+    if ( link.data('toggle') )
+    {
+      return;
+    }
+
+    event.preventDefault();
+    if ( window.location.protocol === 'file:' )
+    {
+      if ( local_destination.substr(-1) === '/' )
+      {
+        local_destination += 'index.html';
+      }
+
+      window.location.href = local_destination;
+      return false;
+    }
+
+    $.get(local_destination, function (data) {
+      var title = $(data).find("title").text();
+      var local_content = $(data).find(".col-sm-9").html();
+      changeUrl(title, local_destination);
+      checkActiveState();
+      $(".col-sm-9").html(local_content);
+      //If has a class hash (jump on the page)
+      if(link.hasClass('hash')){
+        var local_destination_array = local_destination.split('#');
+        var paragraph_id = local_destination_array[1];
+        var paragraph_destination = $('.col-sm-9 #' + paragraph_id).offset().top;
+        $('body,html').animate({scrollTop: paragraph_destination + "px"});
+      }
+      // Set height
+      var content_height = $(".col-sm-9").height();
+      $(".col-sm-3").css("min-height", content_height + "px");
+    });
+
+    setTimeout(function() {
+      linkAnchors();
+    },250);
+
+  });
+  linkAnchors();
+});
 
 function linkAnchors() {
   $('.content-container H3[id], .content-container H4[id], .content-container H5[id], .content-container H6[id]').each(function(idx, el) {
@@ -64,8 +66,19 @@ function changeUrl(title, url) {
 }
 
 function checkActiveState() {
-    var url = window.location.pathname;
-    $(".col-sm-3 ul .active").removeClass("active");
-    $("ul").find("a[href='" + url + "']").addClass('active');
-    $("ul").find("a[href='" + url + "']").closest('.list-group-submenu').parent().children('a').addClass("active");
+  $('UL.nav UL').removeClass('in');
+  $('UL A').each(function(idx, a) {
+    var $a = $(a);
+    if ( a.href === window.location.href )
+    {
+      $a.addClass('active');
+      $a.closest('.list-group-submenu-submenu').parent().children('a').addClass("active");
+      $a.closest('.list-group-submenu').parent().children('a').addClass("active");
+      $a.parents('UL').addClass('in');
+    }
+    else
+    {
+      $a.removeClass('active');
+    }
+  });
 }
