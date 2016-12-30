9b2dc6c913316a551bf9785bb4b65b4c4fa033b3
Vincent Fiduccia <vincent@rancher.com>
2016-12-30 14:52:30 -0700

    Nav updates

--- js/rancher-docs.js
+++ js/rancher-docs.js
@@ -1,21 +1,46 @@
 $(document).ready(function(){
-  $("#menu li a").click(function(){
-    var link = $(this);
-    var closest_ul = link.closest("ul");
-    var parallel_active_links = closest_ul.find(".active")
-    var closest_li = link.closest("li");
-    var link_status = closest_li.hasClass("active");
-    var count = 0;
+  var $menu = $("#menu");
 
-    closest_ul.find("ul").slideUp(function(){
-      if(++count == closest_ul.find("ul").length)
-        parallel_active_links.removeClass("active");
-    });
+  $menu.slicknav({
+    label: "Navigation",
+    duration: 250,
+    appendTo: "logo"
+  });
 
-    if(!link_status)
-    {
-      closest_li.children("ul").slideDown();
-      closest_li.addClass("active");
+  // Expand the tree to the current URL
+  var found = false;
+  $menu.find("a").each(function(idx, link) {
+    if ( link.href === window.location.href) {
+      expand(link);
+      $(link).addClass('active');
+      found = true;
     }
-  })
-})
+  });
+
+  // Expand items when clicking on the tree
+  $menu.on("click", "li a", function(event) {
+    var open = isExpanded(this);
+    collapseAll();
+    if ( !open ) {
+      expand(this);
+    }
+
+    // Don't change the URL for links that are just an anchor like "#hosts"
+    if ( $(this).attr('href').substr(0,1) === '#' ) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  });
+});
+
+function isExpanded(link) {
+  return $(link).closest("li").hasClass("active");
+}
+
+function expand(link) {
+  $(link).parentsUntil("#menu","li").addClass("active");
+}
+
+function collapseAll() {
+  $('#menu .active').removeClass('active');
+}
